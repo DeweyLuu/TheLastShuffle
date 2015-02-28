01813534999fc8d88bf346ab11e01b36728bc54d
Dewey Luu <deweyluu@gmail.com>
2015-02-27 23:07:25 -0800

    Update lastshuffle.js

--- lastshuffle.js
+++ lastshuffle.js
@@ -16,17 +16,17 @@ $(document).ready(function() {
 		function Shoe() {
 			var thisShoe = this;
 			var kicks = new Array();
-				kicks[0] = document.getElementById('jthree').innerHTML = '<img src="jordan3.jpg" />';
-				kicks[1] = document.getElementById('jfour').innerHTML = '<img src="jordan4.jpg" />';
-				kicks[2] = document.getElementById('jfive').innerHTML = '<img src="jordan5.jpg" />';
-				kicks[3] = document.getElementById('jsix').innerHTML = '<img src="jordan6.jpg" />';
-				kicks[4] = document.getElementById('jseven').innerHTML = '<img src="jordan7.jpg" />';
-				kicks[5] = document.getElementById('jeight').innerHTML = '<img src="jordan8.jpg" />';
-				kicks[6] = document.getElementById('jten').innerHTML = '<img src="jordan10.jpg" />';
-				kicks[7] = document.getElementById('jeleven').innerHTML = '<img src="jordan11.jpg" />';
-				kicks[8] = document.getElementById('jtwelve').innerHTML = '<img src="jordan12.jpg" />';
-				kicks[9] = document.getElementById('jthirteen').innerHTML = '<img src="jordan13.jpg" />';
-				kicks[10] = document.getElementById('jfourteen').innerHTML = '<img src="jordan14.jpg" />';
+				kicks[0] = document.getElementById('jthree').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/679zanqe2dgs37d/jordan3.jpg" />';
+				kicks[1] = document.getElementById('jfour').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/tr6wptbk6bs971g/jordan4.jpg" />';
+				kicks[2] = document.getElementById('jfive').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/vf30kds9djcvch8/jordan5.jpg" />';
+				kicks[3] = document.getElementById('jsix').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/1vv71cvijjqvb71/jordan6.jpg" />';
+				kicks[4] = document.getElementById('jseven').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/wrxefc4l41dyshv/jordan7.jpg" />';
+				kicks[5] = document.getElementById('jeight').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/1kx13abdj6tdmo3/jordan8.jpg" />';
+				kicks[6] = document.getElementById('jten').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/ken50fgwgehk8ii/jordan10.jpg" />';
+				kicks[7] = document.getElementById('jeleven').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/rop452o4byuh5ye/jordan11.jpg" />';
+				kicks[8] = document.getElementById('jtwelve').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/ohab75cu540jj94/jordan12.jpg" />';
+				kicks[9] = document.getElementById('jthirteen').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/sbzhx8jjocd621x/jordan13.jpg" />';
+				kicks[10] = document.getElementById('jfourteen').innerHTML = '<img src="https://dl.dropboxusercontent.com/s/xaz1agsgvixe4d0/jordan14.jpg" />';
 			this.ranks = [kicks[0], kicks[1], kicks[2], kicks[3], kicks[4], kicks[5], kicks[6], kicks[7], kicks[8], kicks[9], kicks[10]];
 
 			$.each(thisShoe.ranks, function() {
